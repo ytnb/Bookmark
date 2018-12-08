d88eec1190ee616429c7baf6460fdfdbfcb50cdf
ytnb <ytnbops@gmail.com>
2018-12-08 20:28:10 +0900

    fix #1 industryIdentifiers is null filter logic add

--- app/src/main/java/jp/androidbook/bookmark/viewmodels/BookSearchViewModel.kt
+++ app/src/main/java/jp/androidbook/bookmark/viewmodels/BookSearchViewModel.kt
@@ -1,13 +1,32 @@
 package jp.androidbook.bookmark.viewmodels
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.*
 import jp.androidbook.bookmark.data.Book
 import jp.androidbook.bookmark.data.api.GoogleBookClients
 
 class BookSearchViewModel : ViewModel() {
 
-    fun bookSearchResult(): LiveData<Book> {
+    private val title: MutableLiveData<String> = MutableLiveData()
+    val bookSearchResult: MediatorLiveData<Book> = MediatorLiveData()
+
+    init {
+        title.value = ""
+        val list = Transformations.switchMap(title) { title ->
+            Transformations.map(bookSearch(title)) { book ->
+                val items = book.items.filter {
+                    !it.volumeInfo.industryIdentifiers.isNullOrEmpty()
+                }
+                Book(book.totalItems, items)
+            }
+        }
+        bookSearchResult.addSource(list, bookSearchResult::setValue)
+    }
+
+    fun setTitle(title: String?) {
+        this.title.value = title ?: ""
+    }
+
+    private fun bookSearch(title: String): LiveData<Book> {
         // TODO titleをもらい[intitle:]を追加してapi問い合わせする
         return GoogleBookClients.getBookFromTitle("intitle:Android")
     }
